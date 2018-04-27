5a76488da434efd8cd80e50d2a1053a9e1d3d7d1
Mike Corbett <srcorbetto@gmail.com>
2018-04-26 21:59:54 -0500

    sass setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import "./stylesheets/main.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
