37c6cc2176e1e6e0c233d43358ff4824f98b2891
brothernk <brothernk21@hotmail.com>
2018-04-26 22:19:13 -0500

    sass additions

--- client/src/index.js
+++ client/src/index.js
@@ -2,4 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+import "./stylesheets/main.css";
+
 ReactDOM.render(<App />, document.getElementById("root"));
