d3ba88a667237d60a4629dbaa2f91627b86b7b25
brothernk <brothernk21@hotmail.com>
2018-04-27 09:49:39 -0500

    button styling

--- client/src/components/Buttons/enterButton.js
+++ client/src/components/Buttons/enterButton.js
@@ -3,7 +3,8 @@ import "./button.css"
 
 export const EnterBtn = props => (
 
-    <span {...props} id = {props.id} className="btn btn-success signup-btn">
+    <span {...props} id = {props.id} className="btn enter-btn">
+    {/* btn btn-success signup-btn */}
         Enter Game
     </span>
 );
--- client/src/components/Buttons/loginButton.js
+++ client/src/components/Buttons/loginButton.js
@@ -2,8 +2,9 @@ import React from "react";
 import "./button.css"
 
 export const LoginBtn = props => (
-    <span {...props} id = {props.id} className="btn btn-success login-btn">
-    Enter Game
+    <span {...props} id = {props.id} className="btn login-btn">
+    {/* btn btn-success login-btn */}
+    Join Game
   </span>
 )
 
