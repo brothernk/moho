f1eac965f9a3b132b1887c1965015367a52e08f6
mikelsito <mikelsito@gmail.com>
2018-04-28 13:00:37 -0500

    ...

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
-const server = require('http').createServer(app)
+const server = require('http').Server(app)
 const io = require('socket.io').listen(server);
 const PORT = process.env.PORT || 3001;
 
@@ -30,6 +30,6 @@ io.on('connection', function (socket) {
 });
 
 // Start the API server
-app.listen(PORT, function() {
+server.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
