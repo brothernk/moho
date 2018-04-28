3125fff7205b60aeaf5b08c87c024c23372fe495
mikelsito <mikelsito@gmail.com>
2018-04-28 11:02:13 -0500

    added socket dependency to server and client

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const routes = require("./routes");
+const io = require('socket.io');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -18,6 +19,17 @@ app.use(routes);
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/gifcategories", function(err) {
   if (err) throw err;
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  // socket.on('chat message', function(msg){
+  //   io.emit('chat message', msg);
+  //   console.log('message: '+ msg);
+  // });
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 });
 
 // Start the API server
