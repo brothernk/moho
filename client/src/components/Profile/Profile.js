2b3e9d97e429927e960d883a41820fd58c0434ab
brothernk <brothernk21@hotmail.com>
2018-04-28 12:56:29 -0500

    buttons on prof page

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -4,10 +4,20 @@ export const Profile = props => (
     <div>
         <div className="setup-profile">
             <div className="enter-profile">Setup Your Profile</div>
-            <input type="text"{...props}/>
+            <input type="text"{...props} placeholder="Enter Name"/>
         </div>
         <div className="setup-color">
             <div className="enter-color">Pick a Color</div>
+        </div>
+        <div className="setup-color-buttondiv">
+            <span id="yellow-prof" className="btn color-btn"></span>
+            <span id="blue-prof" className="btn color-btn"></span>
+            <span id="red-prof" className="btn color-btn"></span>
+            <span id="pink-prof" className="btn color-btn"></span>
+            <span id="green-prof" className="btn color-btn"></span>
+            <span id="orange-prof" className="btn color-btn"></span>
+            <span id="purple-prof" className="btn color-btn"></span>
+            <span id="charcoal-prof" className="btn color-btn"></span>
         </div>
     </div>
 );
