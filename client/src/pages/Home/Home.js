d0ee25d3f9d6e1efba14dc4f05e46112b9fed774
Julia Nissen <nisse053@umn.edu>
2018-05-02 18:55:46 -0500

    ip verification

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -11,21 +11,28 @@ class Home extends Component {
     state = {
         urlString: "",
         profileName: "",
-        showProfile: false
+        ipAddress: "",
+        showProfile: false,
+        showHome: false
     }
 
+    // check IP address on mount
     componentDidMount = () => {
-        this.setUrl()
         this.checkIp()
     }
 
+    // Grab user IP address set state variable, then continue to set URL state variable
     checkIp = () => {
-       lookup()
-       .then(function(info) {
-           console.log(info)
-       })
+        
+        lookup()
+        .then((info) => {
+            this.setState({ipAddress: info.ip})
+            this.setUrl()
+        })
+        
     }
 
+    // Grab current URL and set state variable, then continue to check URL
     setUrl = () => {
         let currenturl = window.location.href;
         let spliturl = currenturl.split("/");
@@ -40,46 +47,73 @@ class Home extends Component {
         })
     }
 
-    callGIPHY = () => {
-        API.getGIF(this.state.searchTerm)
-        .then(response => {
-            console.log(response.data.data);
-        })
-        .catch(err => console.log(err))
-    };
-
+    // Check state variable URL against session database. If url exist is database, the user can continue into the game
+    // If the URL does not exist in the database, the user is redirected to an error screen
     checkURL = () => {
 
         API.checkSessionUrl(this.state.urlString)
         .then(res =>{ 
+            // If URL does not exist, user gets error screen
             if (res.data.length < 1) {
                 console.log("Not found")
                 window.location.href = "/notfound"
                 return false
             }
 
+            // If URL does exist
             else {
                 console.log(res.data)
                 console.log("You entered a valid session!")
+
+                // If no members yet exist in session, user is shown profile page
+                if (res.data[0].members.length === 0 ) {
+                    console.log("no members yet in session")
+                    this.setState({showProfile: true})
+                }
+                
+                // If users exist but user's ip address is not associated with a session member,
+                // user gets shown profile page. If the ip address already exists, user goes straight to 
+                // home page
+                for (var i = 0; i < res.data[0].members.length; i ++) {
+                    if (res.data[0].members[i].ip === this.state.ipAddress) {
+                        console.log("member already exists in session ") 
+                        this.setState({showProfile: false})
+                        this.setState({showHome: true})
+                        break
+                    }
+
+                    else {
+                        console.log("member does not yet exist in session")
+                        this.setState({showProfile: true})
+                    }
+                }
+
+
+            
             }
         })
         .catch(err => console.log(err.response));
     };
 
-    handleInputChange = event => {
-        const { name, value } = event.target;
-        this.setState({
-            [name]: value
-        });
-    };
+    profileOnAdd = (field, value) => {
+        this.setState({[field]: value})
+    }
 
     render() {
         return (
             <div> 
-                <Profile />
-                <GiphySearch />
-                <PromptSelect />
-                <BottomNav />
+                { this.state.showProfile ? 
+                    <Profile url={this.state.urlString} ip={this.state.ipAddress} profileAdded={this.profileOnAdd.bind(this)}/>
+                : null}
+
+                { this.state.showHome ?
+                    <div>
+                        <GiphySearch />
+                        <PromptSelect />
+                        <BottomNav />
+                    </div>
+                : null}
+                
             </div>
         );
     }
