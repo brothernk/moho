e13ab704d64358467d54e85cb74bc67ed5546d55
kimb0085 <kathleen.kimble@gmail.com>
2018-05-01 21:28:18 -0500

    added icons to seed file

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -13,17 +13,20 @@ mongoose.connect(
 
 const gifCategories = [
 	{
+		icon: ["<i class="fas fa-exclamation-triangle"></i>"],
 		theme: ["Natural Disasters"],
 		categories: ["Meteor", "Avalanche", "Drought", "Earthquake", "Extinction", "Flood", "Geomagnetic Storms", "Ghost Towns", 
 		"Landslides", "Locust Swarms", "Tornado", "Tsunami", "Volcano", "Wildfire", "Zombie Attack"]
 	},
 	{
+		icon: ["<i class="fab fa-themeisle"></i>"],
 		theme: ["Pirates"],
 		categories: ["Jack Sparrow", "Eye Patch", "Peg Legged", "Ahoy Matey", "Walk The Plank", "Treasure Chest", 
 		"Polly Want a Carrot?", "Skull and Crossbone", "Penzance", "Johnny Depp", "Pirates of the Caribbean", 
 		"Pittsburgh", "Blackbeard", "Jolly Roger", "Grog", "Buried Treasure"]
 	},
 	{
+		icon: ["<i class="fab fa-earlybirds"></i>"],
 		theme: ["Animals"],
 		categories: ["Cats", "Dogs", "Horses", "Pigs", "Chickens", "Tigers", "Bears", "Kangaroo", "Lions", "Meercats",
 		"Cows", "Sloth", "Aardvark", "Badger", "Owl", "Barracuda", "Dingo", "Bat", "Dolphin", "Beetle", "Bison", 
@@ -33,6 +36,7 @@ const gifCategories = [
 		"Stingray", "Wallaby", "Walrus", "Warthog", "Wolverine", "Zebra", "Yak"]
 	},
 	{
+		icon: ["<i class="fas fa-mars-double"></i>"],
 		theme: ["Bromance"],
 		categories: ["Joe Biden and Barack Obama", "Matthew Mcconaughey and Jared Leto", "Mike Corbett and Michael Belsito",
 		"Dane Dehaan and Daniel Radcliffe", "Micahel Cera and Jonah Hill", "Justin Bieber and Usher", "Jay Z and Kanye West",
@@ -44,6 +48,7 @@ const gifCategories = [
 		"George Clooney and Brad Pitt"]
 	},
 	{
+		icon: ["<i class="fas fa-baseball-ball"></i>"],
 		theme: ["Sports"],
 		categories: ["Football", "Archery", "Badminton", "Baseball", "Softball", "Basketball", "Volleyball", "Boxing",
 		"Climbing", "Cycling", "Diving", "Equestrian", "Squash", "Bowling", "Fencing", "Hockey", "Golf", "Gymnastics",
@@ -53,6 +58,7 @@ const gifCategories = [
 		"Downhill Skiing", "Cross Country Skiing", "Skijoring", "Cheerleading"]
 	},
 	{
+		icon: ["<i class="fas fa-gamepad"></i>"],
 		theme: ["90s"],
 		categories: [ "As If!", "Valley Girl", "Wassup", "Getting Jiggy", "Psyche", "Tupac", "Biggie Smalls", "Big Poppa", 
 		"Britney Spears", "Christina Aguilera", "Genie in a Bottle", "Man, I Feel Like a Woman", "Don't Speak", "Michael Jordan",
@@ -65,6 +71,7 @@ const gifCategories = [
 		"Skip-it", "Tomogatchi", "Super Soaker", "Y2K", "Jurassic Park", "Mary Kate and Ashley", "Pokemon"]
 	},
 	{
+		icon: ["<i class="fas fa-pills"></i>"],
 		theme: ["80s"],
 		categories: ["Dallas", "M*A*S*H", "Who Shot JR?", "Video Killed the Radio Star", "MTV", "Prince", "Purple Rain", "Madonna",
 		"Like a Virgin", "CNN", "1984", "Mac or PC", "Gordon Gekko", "Reganomics", "Berlin Wall", "Gorbachev", "David Hasselhoff",
@@ -74,6 +81,7 @@ const gifCategories = [
 		"Be Kind, Rewind", "Rubiks Cube", "Neon", "Teenaged Mutant Ninja Turtles", "Lunchables"]
 	},
 	{
+		icon: ["<i class="fab fa-bitcoin"></i>"],
 		theme: ["2000s"],
 		categories: ["Steve Jobs", "Donald Trump", "Harry Potter", "Britney Spears Meltdown", "Chris Crocker", "Britney Spears and Justin Timberlake",
 		"John Cena", "MTV Reality Shows", "Paris Hilton and Nicole Richie", "Bravo! Reality", "Lady Gaga", "The Daily Show", "Freaks and Greeks",
@@ -85,6 +93,7 @@ const gifCategories = [
 		"Chicago Cubs World Series Champs", "Game of Thrones", "Hamilton", "The Book of Mormon", "Insta Famous", "Adam Rippon", "Fake News"]
 	},
 	{
+		icon: ["<i class="fas fa-snowflake"></i>"],
 		theme: ["Minnesota"],
 		categories: ["Twins", "Vikings", "Wild", "Lynx", "Gophers", "Maroon and Gold", "10,000 Lakes", "Minnesnowta", "Lake Life", "Cabin Life",
 		"Doomtree", "POS", "Dessa", "Atmosphere", "Prince", "Target", "Craft Beer", "Surly", "Mississippi River", "Minnehaha Falls", "Chain of Lakes",
