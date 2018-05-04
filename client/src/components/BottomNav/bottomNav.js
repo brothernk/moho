b8b76ca6660a993c5aaf2895d7905f8c39d8c054
Mike Corbett <srcorbetto@gmail.com>
2018-05-03 20:14:03 -0500

    adjusted bottom nav styles

--- client/src/components/BottomNav/bottomNav.js
+++ client/src/components/BottomNav/bottomNav.js
@@ -4,14 +4,9 @@ import { Link } from 'react-router-dom'
 const BottomNav = props => (
 	<nav className="bottom-nav">
 		<div className="content-holder">
-
-				<span className="fa-stack fa-3x">
-					<i className="fas fa-circle" style={{color:props.userColor}}></i>
-					<strong className="fa-stack-1x" id="username">{props.userName.charAt(0)}</strong>
-				</span>
-
-				<p className="userScore"><i className="fas fa-trophy"></i> {props.userScore}</p>
-				<Link to="/"><i className="fas fa-home"></i></Link>
+		<span className="user-initial" style={{background:props.userColor}}>{props.userName.charAt(0)}</span>
+		<p className="userScore"><i className="fas fa-trophy"></i> {props.userScore}</p>
+		<Link to="/"><i className="fas fa-home home-button"></i></Link>
 		</div>
 	</nav>
 );
