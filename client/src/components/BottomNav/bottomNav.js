3c300c1b6f41c13cc5f1dd916a362445a796b7ae
Mike Corbett <srcorbetto@gmail.com>
2018-05-06 20:45:32 -0500

    cleaned up nav

--- client/src/components/BottomNav/bottomNav.js
+++ client/src/components/BottomNav/bottomNav.js
@@ -5,8 +5,6 @@ const BottomNav = props => (
 	<nav className={props.class}>
 		<div className="content-holder">
 			{/* This will get deleted */}
-			{/* <span className="user-initial" style={{background:props.userColor}}>{props.userName.charAt(0)}</span>
-			<p className="userScore"><i className="fas fa-trophy"></i> {props.userScore}</p> */}
 			<i onClick={props.expand} className="fas fa-bars home-button"></i>
 		</div>
 		{props.children}
