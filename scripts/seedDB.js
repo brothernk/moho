01bddfd3f65b63db2c5d2b44ae02b17c894e4ffd
mikelsito <mikelsito@gmail.com>
2018-05-27 15:23:10 -0400

    Working on DB seeds

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -12,13 +12,13 @@ const gifCategories = [
 		index: 0,
 		color: "#44BBA4",
 		theme: ["That Face When..."],
-		categories: ["You finally get paid", "You see your ex in public", "You realize you left the oven on", "People tell you thath they're counting on you", "You leave work on Friday", "Voldermort kills your parents and leaves you scarred", "You know you shouldn't do something, but you do it anyway", "The drugs start to kick in", "You're drunk and meeting new people", "When you make eye contact with your friend from across the room", "When you're hungry and the cooking instructions say it takes 2 hours", "When you hear your name in public"]
+		categories: ["You finally get paid", "You see your ex in public", "You realize you left the oven on", "People tell you thath they're counting on you", "You leave work on Friday", "Voldermort kills your parents and leaves you scarred", "You know you shouldn't do something but you do it anyway", "The drugs start to kick in", "You're drunk and meeting new people", "When you make eye contact with your friend from across the room", "When you're hungry and the cooking instructions say it takes 2 hours", "When you hear your name in public"]
 	},{
 		icon: "fas fa-university",
 		index: 1,
 		color: "#9964FF",
 		theme: ["State Of The Union"],
-		categories: ["Donald Trump fleeing the media", "Trump's next cabinet member", "Trump struggling to put his tiny fingers in a bowling ball", "George W. Bush trying to read", "Hillary Clinton pandering to black voters", "Bill Clinton not inhaling", "Obama passing the torch", "Trump avoiding the question", "Anti-vaxxers attempting to calmly discuss their beliefs", "Highway protests", ""]
+		categories: ["Donald Trump fleeing the media", "Trump's next cabinet member", "Trump struggling to put his tiny fingers in a bowling ball", "George W. Bush trying to read", "Hillary Clinton pandering to black voters", "Bill Clinton not inhaling", "Obama passing the torch", "Trump avoiding the question", "Anti-vaxxers attempting to calmly discuss their beliefs", "Highway protests", "Sarah Palin spying on Putin from her house", "Bernie Sanders shoplifting candy bars just for the thrill of it", "Jill Stein spikes punch bowl full of LSD at a political gala"]
   },
   {
 		icon: "fas fa-hashtag",
@@ -32,7 +32,7 @@ const gifCategories = [
 		index: 3,
 		color: "#802152",
 		theme: ["Epic Quests"],
-		categories: ["Trying to make it to Taco Bell at 1:58 a.m. when they close at 2:00 a.m.", "A Rogue battling a Wizard"]
+		categories: ["Trying to make it to Taco Bell at 1:58 a.m. when they close at 2:00 a.m.", "A Rogue battling a Wizard", "Waddling around your friends bathroom in search of a new toilet paper roll"]
   },
   {
 		icon: "far fa-frown",
@@ -53,14 +53,14 @@ const gifCategories = [
 		index: 6,
 		color: "#5FACFF",
 		theme: ["Playin It Cool"],
-		categories: ["When you fart in public", "When you almost fall on ice", "When you pull on a push door"]
+		categories: ["When you fart in public", "When you slip on ice", "When you pull on a push door", ]
 	},
   {
 		icon: "fas fa-eye",
 		index: 7,
 		color: "#444444",
 		theme: ["Oh $#@!, It's Behind Me!"],
-		categories: ["When you have to turn the lights off in the basement, and run up the stairs before the monster gets you", "When you're home alone and you hear a noise"]
+		categories: ["When you have to turn the lights off in the basement and run up the stairs before the darkness consumes you", "When you're home alone and you hear a noise"]
   },
   {
 		icon: "fas fa-diagnoses",
