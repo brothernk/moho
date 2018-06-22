a61f1f4361a8bd45166193aa762ae8da1d7ee9ac
Julia Nissen <nisse053@umn.edu>
2018-06-22 14:33:37 -0500

    update

--- scripts/seed.js
+++ scripts/seed.js
@@ -1,19 +1,11 @@
-const mongoose = require("mongoose");
-const db = require("../models");
-mongoose.Promise = global.Promise;
-
-// mongoose.connect(
-// 	process.env.MONGODB_URI || "mongodb://localhost/gifcategories"
-// );
-
-const gifCategories = [
+const Categories = [
 	{
 		icon: "far fa-meh",
 		index: 0,
 		color: "#44BBA4",
 		theme: ["That Face When..."],
 		categories: ["You finally get paid", "You see your ex in public", "You realize you left the oven on", "People tell you thath they're counting on you", "You leave work on Friday", "Voldermort kills your parents and leaves you scarred", "You know you shouldn't do something but you do it anyway", "The drugs start to kick in", "You're drunk and meeting new people", "When you make eye contact with your friend from across the room", "When you're hungry and the cooking instructions say it takes 2 hours", "When you hear your name in public", "You Won free tickets to London! ... Nebraska", "You woke up and wet the bed", "You just made a plate of delicious leftovers and dropped them on floor", "You wait in line at the DMV for 45 minutes only to be told they are closing and you have to come back another time", "You fart in public", "You slip on ice but try to play it cool", "You pull on a push door"]
-	},{
+  },{
 		icon: "fas fa-university",
 		index: 1,
 		color: "#9964FF",
@@ -50,14 +42,4 @@ const gifCategories = [
 	}
 ];
 
-db.Gif
-	.remove({})
-	.then(() => db.Gif.collection.insertMany(gifCategories))
-	.then(data => {
-		console.log(data.insertedIds.length + "categories inserted");
-		process.exit(0);
-	})
-	.catch(err => {
-		console.log(err);
-		process.exit(1);
-});
+module.exports = Categories;
